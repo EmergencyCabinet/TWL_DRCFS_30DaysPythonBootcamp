1b4a21449433c452ae5c435d4a58af2dbe313d78
EmergencyCabinet <prashantpanta00@gmail.com>
2022-12-07 20:07:54 +0545

    Listt

---
+++ Week 1/Personal/Tuples and list.py
@@ -0,0 +1,22 @@
+#list to store multiple users 
+#list is mutable like modify garna milxa 
+
+# tuple 
+#initialization ("something", "something")
+# immubtale .. append haru jasto use garna mildaina 
+# uses less space
+
+#list 
+#initialization [ 1,2 ]
+#mutable ... append replace haru garna milxa 
+
+#tuple
+rainbow_colors = ("red", "violet", "green", "blue","yellow", "indigo")
+
+for color in  enumerate(rainbow_colors):
+    print(color)
+
+index = rainbow_colors.index("red")
+app = rainbow_colors.index("violet")
+print(index)
+print(app)
