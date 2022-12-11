4e3aeeb0fca0deab0bd42aa98ef4f421b2b53d70
EmergencyCabinet <prashantpanta00@gmail.com>
2022-12-11 20:05:51 +0545

    Const

---
+++ Week 2/Personal/Constantt.py
@@ -0,0 +1,11 @@
+#details about mentor and students 
+
+# write variable number in capital for constant like smth you wont change later paxi gayera aba student ko count change hunna ne ta so CAPITAL
+# yo change nagar jasto ko jastai rakhna milxa bhanera aru developer lai ne sajilo banauna ho...multiple thau ma use bhairaxa ani change garna mildaina so CAPTIAL
+
+#normal current_student_count = 60 
+#normal current_mentor_count = 10
+
+CURRENT_STUDENT_COUNT = 60
+CURRENT_MENTOR_COUNT = 10 
+
