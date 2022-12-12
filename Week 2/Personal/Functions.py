2176d4aa85679eded408908061c70c3d9f0cf2d8
EmergencyCabinet <prashantpanta00@gmail.com>
2022-12-12 20:01:50 +0545

    Functionn

---
+++ Week 2/Personal/Functions.py
@@ -0,0 +1,22 @@
+# def le chai add one bhanne function banaunxa 
+# def le function banaunxa 
+# for le logic lagako jasto 
+# function is a block of code that perfons a specific task 
+# call garnu bhanxu function lai run garnu ho 
+# code lai 5 choti copy past garnu bhanda function lai run garnu is thik 
+# return le eta outpur matra nabhayera eta ne leraidinxa 
+
+
+def square(number):
+    result = number * number
+    print ("Result is", result)
+    return result
+
+def greet(name):
+    greet("Prashant")
+    print ("Hello," + name + '!')
+    
+def add_one(num):
+    result = num + 1
+    print(result)
+add_one(5)
